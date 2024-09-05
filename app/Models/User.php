233af4e89c2c6e933dd6d56ac4dbcfb6fdadee39
Tiago Bizze <Tiago Bizze>
2024-09-05 03:20:30 -0300

    API-23: corrigido controller e model do usuário

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function company(): mixed
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
