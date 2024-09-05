9a98deeefb635d999a41873cc8d0c41641e4a18d
Tiago Bizze <Tiago Bizze>
2024-09-05 12:44:51 -0300

    API-24: melhorado seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        $this->call(UserSeeder::class);
+        $this->call([
+            UserSeeder::class,
+            BankAccountSeeder::class,
+        ]);
 
         // \App\Models\User::factory(10)->create();
 
