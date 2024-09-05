6287d2839dc5dcbad12f693bbdae4eab032353ac
Tiago Bizze <Tiago Bizze>
2024-09-05 16:35:43 -0300

    API-25: corrigido campo tipo da view de usuários - create

--- resources/views/users/users-create.blade.php
+++ resources/views/users/users-create.blade.php
@@ -83,7 +83,6 @@
                                     <select
                                         class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                                         id="is_superuser" name="is_superuser">
-                                        <option selected value="">Selecione tipo</option>
                                         @foreach ($tipos as $item)
                                             <option value="{{ $item['id'] }}">{{ $item['name'] }}</option>
                                         @endforeach
