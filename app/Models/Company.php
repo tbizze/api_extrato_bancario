f949ed8a3aa83aa3f413b73fa23cacaed0f55203
Tiago Bizze <Tiago Bizze>
2024-09-05 03:18:22 -0300

    API-23: criado model para empresa

---
+++ app/Models/Company.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function users(): mixed
+    {
+        return $this->hasMany(User::class);
+    }
+
+    // public function bankAccounts()
+    // {
+    //     return $this->hasMany(BankAccount::class);
+    // }
+}
