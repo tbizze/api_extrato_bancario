2ee53e4ed649c9f9223afa2774d2c70ebd3cb9ab
Tiago Bizze <Tiago Bizze>
2024-09-06 01:50:57 -0300

    API-31: criado relacionamento no BankAccount

--- app/Models/BankAccount.php
+++ app/Models/BankAccount.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class BankAccount extends Model
 {
@@ -14,5 +15,10 @@ class BankAccount extends Model
     public function company(): mixed
     {
         return $this->belongsTo(Company::class);
+    }
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
     }
 }
