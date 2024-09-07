574818f568e2cc25e92fd71c5095c7cd8d2bd2db
Tiago Bizze <Tiago Bizze>
2024-09-07 08:42:31 -0300

    API-33: add relacionamento bank no model BankAccount

--- app/Models/BankAccount.php
+++ app/Models/BankAccount.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\{BelongsTo, HasMany};
 
 class BankAccount extends Model
 {
@@ -20,5 +20,9 @@ class BankAccount extends Model
     public function transactions(): HasMany
     {
         return $this->hasMany(Transaction::class);
+    }
+    public function bank(): BelongsTo
+    {
+        return $this->belongsTo(Bank::class);
     }
 }
