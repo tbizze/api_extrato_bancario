02f26ae0e25b4366728e1290d3a8160f74ab1265
Tiago Bizze <Tiago Bizze>
2024-09-08 23:55:51 -0300

    API-33: add método select e melhorias no TransactionImportController

--- app/Http/Controllers/TransactionImportController.php
+++ app/Http/Controllers/TransactionImportController.php
@@ -4,37 +4,61 @@ namespace App\Http\Controllers;
 
 use App\Models\{BankAccount, Transaction};
 use App\Services\TransactionManagerService;
+use Illuminate\Http\{RedirectResponse, Request};
+use Illuminate\View\View;
 
 class TransactionImportController extends Controller
 {
-    protected $transactionManager;
+    protected mixed $transactionManager;
 
     public function __construct(TransactionManagerService $transactionManager)
     {
         $this->transactionManager = $transactionManager;
     }
 
-    public function import(BankAccount $bankAccount)
+    // Método para renderizar view para definir parâmetros para importação de transações.
+    public function select(BankAccount $bankAccount): View
     {
-        //$bankAccount = BankAccount::findOrFail($request->input('bank_account_id'));
+        // Passa o BankAccount para a view.
+        // para mostrar qual conta bancária estará solicitando a importação.
+        return view('transactions.import', compact('bankAccount'));
+    }
 
-        // Importa as transações do banco associado
-        $transactions = $this->transactionManager->importTransactions($bankAccount);
-        //dd($transactions, 'complement');
+    // Método para receber da view o pedido de importação de transações.
+    public function import(BankAccount $bankAccount, Request $request): RedirectResponse
+    {
+        // Regras de validação para a data inicial de importação.
+        $rules = [
+            'initial_date' => 'required|date',
+        ];
 
-        // Salvar as transações no banco de dados
-        //Transaction::insert($transactions);
-
-        $x = 0;
-
-        foreach ($transactions as $item) {
-            $x++;
-            dump($item);
-            Transaction::create($item);
+        // Regras de validação para data final de importação.
+        if ($bankAccount->bank->id == 1) {
+            $rules['final_date'] = 'required|date'; // Torna obrigatório se 1 --> Santander
+        } else {
+            $rules['final_date'] = 'nullable|date'; // Deixa opcional se 2 --> PagBank
         }
 
-        //dd($x);
+        // Validação de dados
+        $validated = $request->validate($rules);
 
-        return redirect()->route('bank-accounts.transactions.index', $bankAccount)->with('success', 'Transações importadas com sucesso!');
+        // Chama o serviço que gerencia requisições às APIs dos diversos bancos.
+        // Passa os parâmetros recebidos.
+        $transactions = $this->transactionManager
+            ->importTransactions($bankAccount, $validated['initial_date'], $validated['final_date']);
+
+        // Verifica o retorno do TransactionManagerService.
+        if (isset($transactions) && is_array($transactions) && count($transactions) > 0) {
+            // Se retornado transações, salva do banco de dados.
+            Transaction::insert($transactions);
+            $number_transactions_import = count($transactions);
+        } else {
+            // Se não retornou transações, emite uma mensagem avisando.
+            return redirect()->route('bank-accounts.transactions.select', $bankAccount)->with('message', "Não foi encontrado transações para importar!");
+        }
+
+        // Redireciona para lista de transações. Exibe mensagem de sucesso.
+        return redirect()->route('bank-accounts.transactions.index', $bankAccount)
+            ->with('success', "Foram importadas $number_transactions_import transações com sucesso!");
     }
 }
