3044186c51d5e0128e596a7222dfb8b3c1dcb0ed
Tiago Bizze <Tiago Bizze>
2024-09-10 19:42:46 -0300

    API-34: update view create/edit de bank-accounts - select bancos

--- resources/views/bank-accounts/create.blade.php
+++ resources/views/bank-accounts/create.blade.php
@@ -9,29 +9,63 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{-- Chama compomente para exibir flesh message --}}
+                    <x-flash-message />
+
                     <div class="flex justify-between items-center">
                         <h1 class="py-5 text-xl">Nova Conta Bancária</h1>
                     </div>
 
                     <form action="{{ route('bank-accounts.store') }}" method="POST" class="w-full max-w-lg">
                         @csrf
-                        {{-- @method('POST') --}}
+                        {{-- Linha 1 --}}
+                        <div class="flex flex-wrap -mx-3 mb-6">
+                            <div class="w-full px-3">
+                                <label class="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2"
+                                    for="bank_id">
+                                    Banco
+                                </label>
+                                <div class="relative">
+                                    <select
+                                        class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                        id="bank_id" name="bank_id">
+                                        <option selected value="">Selecione banco</option>
+                                        @foreach ($banks as $item)
+                                            <option value="{{ $item['id'] }}">{{ $item['bank_name'] }}</option>
+                                        @endforeach
+                                    </select>
+                                    <div
+                                        class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20">
+                                            <path
+                                                d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                                        </svg>
+                                    </div>
+                                </div>
+                                @error('bank_id')
+                                    <div class="text-xs text-red-600">{{ $message }}</div>
+                                @enderror
 
+                            </div>
+                        </div>
+                        {{-- Linha 2 --}}
                         <div class="flex flex-wrap -mx-3 mb-6">
                             <div class="w-full px-3">
                                 <label class="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2"
                                     for="bank_name">
-                                    Nome do Banco
+                                    Descrição
                                 </label>
                                 <input
                                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                                     type="text" id="bank_name" name="bank_name" value="{{ old('bank_name') }}"
-                                    placeholder="Banco do Brasil">
+                                    placeholder="Registre uma descrição">
                                 @error('bank_name')
                                     <div class="text-xs text-red-600">{{ $message }}</div>
                                 @enderror
                             </div>
                         </div>
+                        {{-- Linha 3 --}}
                         <div class="flex flex-wrap -mx-3 mb-6">
                             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label class="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2"
@@ -60,6 +94,7 @@
                                 @enderror
                             </div>
                         </div>
+                        {{-- Botões --}}
                         <div class="flex gap-4">
                             <button type="submit"
                                 class="rounded border-slate-700 bg-slate-700 py-3 px-5">Salvar</button>
