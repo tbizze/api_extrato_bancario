70465a59d8e6b5ecc6aeaa1556fd5e25c0d33881
Tiago Bizze <Tiago Bizze>
2024-09-10 19:31:55 -0300

    API-34: criado AccessTokenController

---
+++ app/Http/Controllers/AccessTokenController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AccessToken;
+use Illuminate\Http\Request;
+
+class AccessTokenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): void
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): void
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): void
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AccessToken $accessToken): void
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AccessToken $accessToken): void
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AccessToken $accessToken): void
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AccessToken $accessToken): void
+    {
+        //
+    }
+}
