48e0b91c7d95cbe8d4d56ec0a1da37cb8983aa18
Tiago Bizze <Tiago Bizze>
2024-09-13 13:57:27 -0300

    API-36: update schedule para todo dia / 08:00

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\ImportBankTransactionsJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,6 +14,15 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+
+        // Agendar o Job executar: XXXXX, às 08:00
+        // weekdays -> de segunda a sexta | daily -> diário.
+        $schedule->job(new ImportBankTransactionsJob())
+            ->daily()
+            ->at('08:00');
+
+        //Agendar o Job executar a cada minuto.
+        //$schedule->job(new ImportBankTransactionsJob())->everyMinute();
     }
 
     /**
