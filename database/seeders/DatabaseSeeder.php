bdebd00e24d9c3ccc2be1d766b5ffae331fb0624
Tiago Bizze <Tiago Bizze>
2024-09-20 21:58:48 -0300

    API-47: add BankSeeder no DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UserSeeder::class,
+            BankSeeder::class,
             // BankAccountSeeder::class,
             // TransactionSeeder::class,
         ]);
