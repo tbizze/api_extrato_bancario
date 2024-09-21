74090ef276f848f4e42152d46e15c17841feb669
Tiago Bizze <Tiago Bizze>
2024-09-20 21:57:27 -0300

    API-47: alterado coluna cnpj para nullable da migration companies

--- database/migrations/2024_09_05_021247_create_companies_table.php
+++ database/migrations/2024_09_05_021247_create_companies_table.php
@@ -13,7 +13,7 @@ return new class () extends Migration {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('cnpj', 14);
+            $table->string('cnpj', 14)->nullable();
             $table->timestamps();
         });
     }
